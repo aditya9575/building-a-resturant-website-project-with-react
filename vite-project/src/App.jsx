384e4d40a1d71c3f7c83242959e3a4d7add276f6
aditya mehto <adityamehto19@gmail.com>
2024-06-10 11:25:50 +0530

    5th new commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeaderComponent from './components/Layout/HeaderComponent'
 import Meals from './components/Meals/Meals'
+import Cart from './components/Cart/Cart'
 
 const App = () => {
   return (
     <div>
+    <Cart/>
       <HeaderComponent/>
       <main>
         <Meals/>
