9ad3b0f13b2fc405451a2ede1e43d78bc367cfae
aditya mehto <adityamehto19@gmail.com>
2024-06-10 11:55:07 +0530

    6th commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HeaderComponent from './components/Layout/HeaderComponent'
 import Meals from './components/Meals/Meals'
 import Cart from './components/Cart/Cart'
 
 const App = () => {
+
+const [cartIsShown , setCartIsShown] = useState(false);
+
+const showCartHandler = () => {
+  setCartIsShown(true);
+}
+
+const hideCartHandler = () =>{
+  setCartIsShown(false)
+}
+
   return (
     <div>
-    <Cart/>
-      <HeaderComponent/>
+    {cartIsShown && <Cart onCartCloseButtonPress={hideCartHandler}/>}
+
+      <HeaderComponent onCartButtonPress={showCartHandler}/>
+    
       <main>
         <Meals/>
       </main>
--- vite-project/src/components/Layout/HeaderComponent.jsx
+++ vite-project/src/components/Layout/HeaderComponent.jsx
@@ -3,7 +3,7 @@ import classes from "../Layout/HeaderComponent.module.css";
 import headerImage from "../../assets/meals.jpg";
 import HeaderCartButtonComponent from "./HeaderCartButtonComponent";
 
-const HeaderComponent = () => {
+const HeaderComponent = (props) => {
   return (
     <Fragment>
       {/* part 1 */}
@@ -12,7 +12,7 @@ const HeaderComponent = () => {
         <h1>DineWithReact</h1>
 
         {/* our custom button */}
-        <HeaderCartButtonComponent/>
+        <HeaderCartButtonComponent onClick={props.onCartButtonPress}/>
       </header>
 
         {/* part 2 this holds image */}
